8da5b9fc24579b97580dc999ff6963d45d994912
potof <slptmac@gmail.com>
2021-05-17 00:08:34 +0900

    add DB

--- controllers.py
+++ controllers.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
+import db
+from models import User, Task
 
 app = FastAPI(
     title='FastAPIでつくるtoDoアプリケーション',
@@ -21,7 +23,11 @@ def index(request: Request):
 
 # 管理画面
 def admin(request: Request):
+    user = db.session.query(User).filter(User.username == 'admin').first()
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    db.session.close()
+ 
     return templates.TemplateResponse('admin.html',
                                       {'request': request,
-                                       'username': 'admin'})
-
+                                       'user': user,
+                                       'task': task})
